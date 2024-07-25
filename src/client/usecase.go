44c13002c81c82c96bccce113044bce7b4e20508
Yusuf Talha Kılıç <talha.kilic@priviasecurity.com>
2024-07-25 16:03:16 +0300

    initial

---
+++ src/client/usecase.go
@@ -0,0 +1,28 @@
+package client
+
+import (
+	"config"
+	"log"
+	"net/http"
+)
+
+type UseCase interface {
+	MakeRequest(TCKimlik *TCKimlik) bool
+}
+
+type Client struct {
+	HttpClient *http.Client
+	Config     *config.ServiceConfig
+}
+
+func NewClient() UseCase {
+	serviceConfig, err := config.Load()
+	if err != nil {
+		log.Fatal(err)
+	}
+	c := &http.Client{}
+	return &Client{
+		HttpClient: c,
+		Config:     serviceConfig,
+	}
+}
